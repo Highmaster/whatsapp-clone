9a9e15a27d60956a9d9ef5e962afa45c236c52bf
Highmaster <nzechideraann@gmail.com>
2025-05-05 22:41:25 +0100

    Added MessageMapper service to map Message entity to MessageResponse DTO.

---
+++ src/main/java/com/highmaster/whatsappclone/message/MessageMapper.java
@@ -0,0 +1,23 @@
+package com.highmaster.whatsappclone.message;
+
+import com.highmaster.whatsappclone.file.FileUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageMapper {
+    public MessageResponse toMessageResponse(Message message) {
+        return MessageResponse.builder()
+                .id(message.getId())
+                .content(message.getContent())
+                .senderId(message.getSenderId())
+                .receiverId(message.getReceiverId())
+                .type(message.getType())
+                .state(message.getState())
+                .createdAt(message.getCreatedDate())
+                .media(FileUtils.readFileFromLocation(message.getMediaFilePath()))
+                //todo read the media file
+                .build();
+    }
+
+
+}
